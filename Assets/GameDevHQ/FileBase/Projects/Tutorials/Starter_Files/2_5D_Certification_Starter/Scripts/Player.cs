1739d68a888e8be8c0e65cd6150405e6d5e9d509
ZeusK10 <neeraz.karkee18@gmail.com>
2021-06-22 04:27:43 +0545

    Added idle run and jump animation

--- Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
+++ Assets/GameDevHQ/FileBase/Projects/Tutorials/Starter_Files/2_5D_Certification_Starter/Scripts/Player.cs
@@ -8,11 +8,13 @@ public class Player : MonoBehaviour
     [SerializeField]
     private float _speed=2.0f,_jumpHeight=15.0f,_gravity=1.0f,_yVelocity;
     private Vector3 move, velocity;
+    private Animator _playerAnimator;
 
     // Start is called before the first frame update
     void Start()
     {
         _controller = GetComponent<CharacterController>();
+        _playerAnimator = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,10 +23,21 @@ public class Player : MonoBehaviour
 
         if(_controller.isGrounded==true)
         {
+            _playerAnimator.SetBool("IsJumping", false);
             float horizontalInput = Input.GetAxis("Horizontal");
             move = new Vector3(0, 0, horizontalInput);
             velocity = move * _speed;
+            if(horizontalInput!=0)
+            {
+                Vector3 flip = transform.localEulerAngles;
+                flip.y = move.z > 0 ? 0 : 180;
+                transform.localEulerAngles = flip;
+            }
+            
+            _playerAnimator.SetFloat("Speed", Mathf.Abs(horizontalInput));
 
+            
+            
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 _yVelocity = _jumpHeight;
@@ -32,6 +45,7 @@ public class Player : MonoBehaviour
         }
         else
         {
+            _playerAnimator.SetBool("IsJumping", true);
             _yVelocity -= _gravity;
         }
         velocity.y = _yVelocity;
